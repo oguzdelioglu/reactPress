ccbafd5dd9ed93556b917c478f1c8d3098237e29
ODEL <oguzdelioglu@gmail.com>
2022-10-14 17:08:45 +0300

    API Service Updated

--- src/services/server/index.js
+++ src/services/server/index.js
@@ -1,16 +1,9 @@
-// const { addPost } = require('../firebase/index.js');
-// import { addPost } from '../firebase/index.js'
-// module.exports = require("../firebase.js");
-// const parse = require('./firebase');
-// const {addPost} = require('./firebase');
 const { initializeApp } = require("firebase/app");
-const { getFirestore, addDoc, collection,Timestamp, getDocs,query,orderBy,limit, startAfter, where } = require('firebase/firestore');
+const { getFirestore, doc, addDoc , setDoc, collection,Timestamp, getDocs,query,orderBy,limit, startAfter, where } = require('firebase/firestore');
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const _ = require('lodash');
-
-
-//Firestore
 const firebaseApp = initializeApp({
   apiKey: "AIzaSyCJ7DLBntXHYdsX2EkLFX19ogvwe7lcq9g",
   authDomain: "edelsteineinformationen.firebaseapp.com",
@@ -21,72 +14,88 @@ const firebaseApp = initializeApp({
   measurementId: "G-E08C944VG0"
 });
 const db = getFirestore(firebaseApp);
-
-// const functions = require('firebase-functions');
-// const admin = require('firebase-admin')
-// const db = admin.firestore();
-
-const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.get('/api', (req, res) => {
-  res.send({ express: 'Powered by Oğuz DELİOĞLU' });
+  res.send({ author: 'Powered by Oğuz DELİOĞLU' });
 });
-
-app.post('/api/post/add', async (req, res) => {
-  let payload = req.body;
-  let keys = Object.keys(payload);
-  let obj = {};
-  let i = 0;
-  try {
-    _.forEach(payload, async data => {
-      obj[keys[i]] = data;
-      i++;
-    })
-
-
-    //Decide hit if not exist
-    if(obj.hit == null) {
-      obj.hit = 0
-    }
-    //Add Timestamp if null 
-    if(obj.date == null || obj.date === '') {
-      obj.date = Timestamp.now()
-    }
-
-    console.log(obj)
-    const result = await addPost(obj)
-   //await db.collection('users').doc().set(obj);
-   if(result && result.id) {
-    res.status(200).send('Post Added:' + result.id);
-   } else {
-    res.status(402).send('Post Not Added:' + result);
-   }
-  } catch(err) { res.send(JSON.stringify(err))}
-
-  // console.log(req.body);
-  // const data = req.body;
-  // if(data && data.title && data.content && data.category && data.date && data.hit && data.image && data.link && data.published && data.tags) {
-  //   res.send(`I received your POST request. This is what you sent me:  ${JSON.stringify(data)}`,);
-  // } else {
-  //   res.send(`Your Post Data is not compatible:  ${JSON.stringify(data)}`,);
-  // }
+app.post('/api', async (req, res) => {
+  if(req.body && req.body.TYPE && req.body.data) {
+    let postType = req.body.TYPE;
+    let payload = req.body.data;
+    let keys = Object.keys(payload);
+    let obj = {};
+    let i = 0;
+    try 
+    {
+      _.forEach(payload, async data => {
+        obj[keys[i]] = data;
+        i++;
+      })
+      switch (postType) {
+        case 'ADD_POST':
+          const fixObj = await fixPost(obj)
+          console.log(fixObj)
+          if(!checkPost(fixObj)){
+            res.status(402).send({status:"The data is not enough.",result: fixObj})
+          } else {
+            const result = await addPost(fixObj)
+            if(result && result.id) { //result && result.id
+              res.status(200).send({status:"Post Added",result: result.id})
+            } else {
+              res.status(402).send({status:"Post Not Added",result: result})
+            }
+          }
+          break;
+        default:
+          console.log(`Sorry, we are out of ${postType}.`);
+      }
+      
+     
+    } catch(err) { res.end(JSON.stringify(err))}
+  } else {
+    res.status(200).send({status:"Info",result: "Welcome to API"})
+  }
 });
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Server Listening on port ${port}`));
 
 //Backend
 const addPost = async (data) => {
-  //console.log("Ekleme Başladı:",data)
-  const result = await addDoc(collection(db, "posts"), data);
-  // const result = await db.collection('posts').doc('one').set(data);
-  // const result =  await db.collection('posts').doc().set(data);
-  //console.log("Result:",result);
-  return result;
+  const postCollection = collection(db,"posts");
+  const Snapshot = await getDocs(postCollection);
+  const List = Snapshot.docs.map(doc => doc.data());
+  const post = List.filter(p=> p.link === data.link).shift()
+  if(!post) {
+    const result = await addDoc(postCollection, data);
+    return result
+  } else {
+    return false
+  }
 }
 
+const fixPost = async (obj) => {
+  //Decide hit if not exist
+  if(obj.hit == null) {
+    obj.hit = 0
+  }
+  //Add Timestamp if null 
+  if(obj.date == null || obj.date === '') {
+    obj.date = Timestamp.now()
+  }
+  return obj;
+}
+
+const checkPost = (obj) => {
+  const keyList = ["title","content","category","date","hit","image","link","published","tags"];
+  const isOK = keyList.map(function(key) { return checkKey(obj,key) ? true : false }).find(result => result === false)
+  return isOK === undefined ? true : false;
+}
+
+const checkKey = (obj, keyName) => {
+  let keyExist = Object.keys(obj).some(key => key === keyName);
+  return keyExist;
+};
 
 //https://firebase.google.com/docs/firestore/manage-data/add-data#web-version-9_3  For Firestore Commands
