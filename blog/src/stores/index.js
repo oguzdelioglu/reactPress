19669dcfa73ba9b9d6aaa84836ceffc2818a15de
ODEL <oguzdelioglu@gmail.com>
2022-10-22 02:44:13 +0300

    Updated

--- blog/src/stores/index.js
+++ blog/src/stores/index.js
@@ -1,12 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import globalSlice from "./global";
-// import postsSlice from './posts';
-// import postSlice from './post';
 
 export default configureStore({
   reducer: {
-    global: globalSlice,
-    // posts: postsSlice,
-    // post: postSlice
+    global: globalSlice
   },
 })
